e10bdbb441a02d097fae52c4caffc108a8ef2307
Alexandr <alexber59rus@gmail.com>
2020-03-11 23:49:35 +0500

    Add admin form
    Добавлено администрирование системы

--- App_Normocontrol/admin.py
+++ App_Normocontrol/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Work, School, Students, Group, Teachers
+admin.site.register(Work)
+admin.site.register(School)
+admin.site.register(Students)
+admin.site.register(Group)
+admin.site.register(Teachers)
--- App_Normocontrol/models.py
+++ App_Normocontrol/models.py
@@ -1,6 +1,3 @@
-from django.db import models
-
-# Create your models here.
 # This is an auto-generated Django model module.
 # You'll have to do the following manually to clean this up:
 #   * Rearrange models' order
@@ -8,15 +5,21 @@ from django.db import models
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
 
 
 class Group(models.Model):
     group_id = models.IntegerField(db_column='Group_id', primary_key=True)  # Field name made lowercase.
     group = models.CharField(db_column='Group', max_length=5)  # Field name made lowercase.
 
+    def __str__(self):
+        return self.group
+
     class Meta:
         managed = False
         db_table = 'Group'
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
 
 
 class Meeting(models.Model):
@@ -26,7 +29,10 @@ class Meeting(models.Model):
     student = models.ForeignKey('Students', models.DO_NOTHING, db_column='Student')  # Field name made lowercase.
     teacher = models.ForeignKey('Teachers', models.DO_NOTHING, db_column='Teacher')  # Field name made lowercase.
     meeting_status = models.ForeignKey('MeetingStatus', models.DO_NOTHING, db_column='Meeting_status')  # Field name made lowercase.
-
+    #
+    # def __str__(self):
+    #     return self.student + self.teacher + self.meeting_time
+    #
     class Meta:
         managed = False
         db_table = 'Meeting'
@@ -35,6 +41,9 @@ class Meeting(models.Model):
 class MeetingStatus(models.Model):
     status_id = models.IntegerField(db_column='Status_id', primary_key=True)  # Field name made lowercase.
     status = models.TextField(db_column='Status')  # Field name made lowercase.
+
+    def __str__(self):
+        return self.status
 
     class Meta:
         managed = False
@@ -45,9 +54,14 @@ class School(models.Model):
     school_id = models.IntegerField(db_column='School_id', primary_key=True)  # Field name made lowercase.
     school = models.TextField(db_column='School')  # Field name made lowercase.
 
+    def __str__(self):
+        return self.school
+
     class Meta:
         managed = False
         db_table = 'School'
+        verbose_name = 'Направление'
+        verbose_name_plural = 'Направления'
 
 
 class Students(models.Model):
@@ -60,9 +74,14 @@ class Students(models.Model):
     email = models.TextField(db_column='Email')  # Field name made lowercase.
     login = models.TextField(db_column='Login')  # Field name made lowercase.
 
+    def __str__(self):
+        return str(self.name) + str(self.group)
+
     class Meta:
         managed = False
         db_table = 'Students'
+        verbose_name = 'Студент'
+        verbose_name_plural = 'Студенты'
 
 
 class Teachers(models.Model):
@@ -71,13 +90,18 @@ class Teachers(models.Model):
     password = models.TextField(db_column='Password')  # Field name made lowercase.
     email = models.TextField(db_column='Email')  # Field name made lowercase.
     phone = models.TextField(db_column='Phone')  # Field name made lowercase.
-    meeting_time_begin = models.TimeField(db_column='Meeting_time_begin')  # Field name made lowercase.
-    meeting_time_end = models.TimeField(db_column='Meeting_time_end')  # Field name made lowercase.
+    freetime_b = models.TimeField(db_column='FreeTime_b')  # Field name made lowercase.
+    freetime_e = models.TimeField(db_column='FreeTime_e')  # Field name made lowercase.
     login = models.TextField(db_column='Login')  # Field name made lowercase.
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         managed = False
         db_table = 'Teachers'
+        verbose_name = 'Преподаватель'
+        verbose_name_plural = 'Преподаватели'
 
 
 class Work(models.Model):
@@ -92,6 +116,121 @@ class Work(models.Model):
     literature_criteria = models.TextField(db_column='Literature_criteria')  # Field name made lowercase.
     applications_criteria = models.TextField(db_column='Applications_criteria')  # Field name made lowercase.
 
+    def __str__(self):
+        return self.type_name
+
     class Meta:
         managed = False
         db_table = 'Work'
+        verbose_name = 'Тип работы'
+        verbose_name_plural = 'Типы работы'
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.SmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
